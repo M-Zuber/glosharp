a254ef961152b90ad92425fdae3cd584191a8fea
Hodges, William D <wdhodges@carilionclinic.org>
2019-02-07 21:21:51 -0500

    Initial, barebones commit. :thumbsup:

---
+++ Models/Configuration.cs
@@ -0,0 +1,14 @@
+namespace glosharp.Models
+{
+    /// <summary>
+    /// Configuration
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// Personal Access Token
+        /// </summary>
+        /// <value></value>
+        public string Token { get; set; }
+    }
+}
