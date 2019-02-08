442bab1ec8195badb9260edc23afdd4f8551f2ad
Hodges, William D <wdhodges@carilionclinic.org>
2019-02-07 22:39:37 -0500

    Added GetBoardAsync in Boards.Get

--- Boards/Get.cs
+++ Boards/Get.cs
@@ -11,7 +11,7 @@ namespace Glosharp.Boards
     public class Get
     {
         /// <summary>
-        /// 
+        /// Get all boards
         /// </summary>
         /// <param name="config"></param>
         /// <returns></returns>
@@ -31,11 +31,42 @@ namespace Glosharp.Boards
 
                     var boards = JsonConvert.DeserializeObject<BoardsCollection>(responseString);
 
-                    return new Tuple<bool, string, BoardsCollection>(true, $"{response.StatusCode}", boards);
+                    return new Tuple<bool, string, BoardsCollection>(true, response.StatusCode.ToString(), boards);
                 }
                 catch(HttpRequestException httpException)
                 {
                     return new Tuple<bool, string, BoardsCollection>(false, httpException.Message, null);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get a single board
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="boardId"></param>
+        /// <returns></returns>
+        public async Task<Tuple<bool, string, Board>> GetBoardAsync(Models.Configuration config, string boardId)
+        {
+            using(var client = new HttpClient())
+            {
+                try 
+                {
+                    client.BaseAddress = new Uri("https://gloapi.gitkraken.com/v1/glo/boards/");
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", config.Token);
+
+                    var response = await client.GetAsync(boardId);
+                    response.EnsureSuccessStatusCode();
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    var board = JsonConvert.DeserializeObject<Board>(responseString);
+
+                    return new Tuple<bool, string, Board>(true, response.StatusCode.ToString(), board);
+                }
+                catch(HttpRequestException httpException)
+                {
+                    return new Tuple<bool, string, Board>(false, httpException.Message, null);
                 }
             }
         }
